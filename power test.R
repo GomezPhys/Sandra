7fca44eb45fa0cc957d4d63fd42bc268ed74f81f
Manueliito12 <mgomez26@utep.edu>
2023-12-05 11:58:54 -0700

    ss

--- power test.R
+++ power test.R
@@ -4,10 +4,12 @@ library(pwr)
 
 
 # Parameters
-effect_size <- 0.7
+effect_size <- 0.6
 alpha <- 0.05
-power <- 0.80
+power <- 0.5
+k <- 3 # Number of groups (for a one-way ANOVA)
 
 # Perform power analysis
 pwr.t.test(d = effect_size, sig.level = alpha, power = power, type = "paired")
 
+pwr.anova.test(k = k, f = effect_size, sig.level = alpha, power = power)
